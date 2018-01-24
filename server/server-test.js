35e22cc5f790fb0ad0e94686a4d451f894f45638
peterlxb <peterlxb@gmail.com>
2018-01-24 12:27:21 +0800

    add describe to organize test

--- server/server-test.js
+++ server/server-test.js
@@ -3,25 +3,34 @@ const expect = require('chai').expect;
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).to.include({
-                error:'Page not found',
-                name:'Todo App v1.0'
-            });
-        })
-        .end(done);
-});
+describe('Server',() => {
+    describe('GET /',() => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).to.include({
+                        error: 'Page not found',
+                        name: 'Todo App v1.0'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('should return my user object',(done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).to.be.an('Array');
-        })
-        .end(done);
-});
+    describe('GET /users',() => {
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).to.be.an('Array');
+                })
+                .end(done);
+        });
+    });
+})
+
+
+
--- utils/utils-test.js
+++ utils/utils-test.js
@@ -5,60 +5,76 @@ const utils = require('./utils');
 
 var beverages = { tea: ['chai', 'matcha', 'oolong'] };
 
-it("should add two numbers",() => {
-    var res = utils.add(11,33);
-    //expect(res).to.equal(44).to.be.a('number');
+describe('Utils',() => {
 
-});
+    describe('#add',() => {
+        it("should add two numbers", () => {
+            var res = utils.add(11, 33);
+            //expect(res).to.equal(44).to.be.a('number');
+        });
+     });
 
-//async test use done
-it('should async add two numbers',(done) => {
-    utils.asyncAdd(4,3,(sum) => {
-        expect(sum).to.equal(7).to.be.a('number');
-        done();
-    })
-});
+     describe('#asyncAdd', () => {
+         //async test use done
+         it('should async add two numbers', (done) => {
+             utils.asyncAdd(4, 3, (sum) => {
+                 expect(sum).to.equal(7).to.be.a('number');
+                 done();
+             })
+         });
+     });
 
-it('async square a number',(done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).to.equal(25).to.be.a('number');
-        done();
-    })
-});
-
-it("should square the given number",() => {
-    var res = utils.square(4);
-    expect(res).to.equal(16).to.be.a('number');
-    //expect(res).not.to.equal(14);
-});
-
-it("expect property of object", () => {
-    //expect(beverages).to.have.property('tea').with.lengthOf(3);
-    //expect([1,2,3]).to.include(2);
-    expect({
-        a:1,
-        b:2,
-        c:3
-    }).to.include({
-        b:2
+     describe('#AsyncSquare',() => {
+         it('async square a number', (done) => {
+             utils.asyncSquare(5, (res) => {
+                 expect(res).to.equal(25).to.be.a('number');
+                 done();
+             })
+         });
     });
-});
 
-it('Set the proper username', () => {
-    var user = utils.setName({},"peter liu");
-    expect(user).to.include({firstName:"peter"});
-});
-
-// Example from mocha doc
-/**
- var assert = require('assert');
- describe("Array", function () {
-    describe('#indexOf()', function () {
-        it("should return -1 when the value is not presented!", function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+    describe('#square',() => {
+        it("should square the given number", () => {
+            var res = utils.square(4);
+            expect(res).to.equal(16).to.be.a('number');
+            //expect(res).not.to.equal(14);
         });
     });
-  });
+});
+
+describe('#objTest', () => {
+    it("expect property of object", () => {
+        //expect(beverages).to.have.property('tea').with.lengthOf(3);
+        //expect([1,2,3]).to.include(2);
+        expect({
+            a: 1,
+            b: 2,
+            c: 3
+        }).to.include({
+            b: 2
+        });
+    });
+});
+
+describe('#getUser', () => {
+    it('Set the proper username', () => {
+        var user = utils.setName({}, "peter liu");
+        expect(user).to.include({ firstName: "peter" });
+    });
+})
+
+
+
+// Example from mocha doc
+
+//  var assert = require('assert');
+//  describe("Array", function () {
+//     describe('#indexOf()', function () {
+//         it("should return -1 when the value is not presented!", function () {
+//             assert.equal([1, 2, 3].indexOf(4), -1);
+//         });
+//     });
+//   });
   
- **/
+
 
