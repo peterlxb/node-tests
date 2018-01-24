a22c54fdc66bbec852916fce74dab15539e930aa
peterlxb <peterlxb@gmail.com>
2018-01-24 10:31:58 +0800

    async test

--- utils/utils-test.js
+++ utils/utils-test.js
@@ -7,7 +7,23 @@ var beverages = { tea: ['chai', 'matcha', 'oolong'] };
 
 it("should add two numbers",() => {
     var res = utils.add(11,33);
-    expect(res).to.equal(44).to.be.a('number');
+    //expect(res).to.equal(44).to.be.a('number');
+
+});
+
+//async test use done
+it('should async add two numbers',(done) => {
+    utils.asyncAdd(4,3,(sum) => {
+        expect(sum).to.equal(7).to.be.a('number');
+        done();
+    })
+});
+
+it('async square a number',(done) => {
+    utils.asyncSquare(5, (res) => {
+        expect(res).to.equal(25).to.be.a('number');
+        done();
+    })
 });
 
 it("should square the given number",() => {
